6dfdff4b2687257942510a4b7b94c73118785ab4
brunojares <contato.jares@gmail.com>
2016-02-13 18:08:50 -0300

    Scaffold
    Scaffold da aplicação de teste

---
+++ test/index.js
@@ -0,0 +1,37 @@
+var Server = require('../api/Server/Server');
+
+var _server = new Server({
+	server:{
+		debug: true,
+		//success: function(value){ }
+		//error: function(value){ }
+		//warning: function(value){ }
+		//info: function(value){ }
+	},
+	application: {
+		value: 123
+	}
+});
+
+_server.http(function(application){
+	return {
+		port: 8080,
+		started: function() { },
+		auntenticate: function(session){
+			if(session.request.isHome)
+				return true;
+			else if(session.credential.login == 'jares')
+				return true;
+			else
+				return { code: 0, message: 'User/Password incorrect' };
+		},
+		routes: function(session){
+			return [
+				application.on(session.request.is('client')).directory('./client'),
+				application.on(session.request.is('person')).controller('PersonController')
+			];
+		}
+	};
+});
+
+_server.start();
