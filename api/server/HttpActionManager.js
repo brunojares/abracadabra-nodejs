cd0b0e6b3ed525adfe3d5c229f11c8bce1d6522f
brunojares <contato.jares@gmail.com>
2016-02-29 20:25:59 -0300

    Preparado para fazer o Controller

--- api/server/HttpActionManager.js
+++ api/server/HttpActionManager.js
@@ -7,37 +7,51 @@ module.exports = function(selection, session, config){
 	this.Error404 = function(kind, value){
 		var HttpAction404 = require('./HttpAction404');
 		var _action = new HttpAction404(kind, value);
-		return initAction(_action);
+		return _this.initAction(_action);
 	}
 	this.Error500 = function(error){
 		var HttpAction500 = require('./HttpAction500');
 		var _action = new HttpAction500(error);
-		return initAction(_action);
+		return _this.initAction(_action);
 	}
 	this.TextFile = function(path, mime){
 		var HttpActionTextFile = require('./HttpActionTextFile');
-		var _action = new HttpActionTextFile(path, mime, onError500);
-		return initAction(_action);
+		var _action = new HttpActionTextFile(
+			path, 
+			mime, 
+			function(error){
+				return _this.Error500(error).execute();
+			}	
+		);
+		return _this.initAction(_action);
 	}
 	this.StreamFile = function(path, mime){
 		var HttpActionStreamFile = require('./HttpActionStreamFile');
-		var _action = new HttpActionStreamFile(path, mime, onError500);
-		return initAction(_action);
+		var _action = new HttpActionStreamFile(
+			path, 
+			mime, 
+			function(error){
+				return _this.Error500(error).execute();
+			}
+		);
+		return _this.initAction(_action);
 	}
 	this.JSON = function(object){
 		var HttpActionJSON = require('./HttpActionJSON');
-		var _action = new HttpActionJSON(object, onError500);
-		return initAction(_action);
+		var _action = new HttpActionJSON(
+			object,
+			function(error){
+				return _this.Error500(error).execute();
+			}
+		);
+		return _this.initAction(_action);
 	}	
 
-	function initAction(action){
+	this.initAction = function(action){
 		action.isSelected = selection;
 		action.session = session;
 		action.config = config.application;
 		action.server = config.server;
 		return action;
 	}
-	function onError500(error){
-		return _this.Error500(error).execute();
-	}	
 }
